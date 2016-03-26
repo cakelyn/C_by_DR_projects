1aa9976bebfa9909cd20eb466d94f7c1b75d2f8b
Katelyn Stamas <cakelyn@gmail.com>
2016-03-26 16:42:08 -0400

    now prints the table in reverse order

--- fahr_to_cels/fahr_to_cels/main.c
+++ fahr_to_cels/fahr_to_cels/main.c
@@ -16,7 +16,7 @@ main ()
     
     printf("Fahrenheit\tCelcius\n");
     
-    for (fahr = 0; fahr <= 300; fahr += 20) {
+    for (fahr = 300; fahr >= 0; fahr -= 20) {
         printf("%6d %10.1f\n", fahr, (5.0/9.0) * (fahr-32));
     }
 }
